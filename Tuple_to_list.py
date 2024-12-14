4c43337f592a391449ae0f1098da8f1f00c151b9
Adebanjo Adeoluwadara <adebanjoadeoluwadara@gmail.com>
2024-12-14 15:21:49 -0800

    commit

---
+++ Tuple_to_list.py
@@ -0,0 +1,11 @@
+# Convert single tuple to a list
+my_tuple = ('Adebanjo', 'Dara', 18, '5 feet, 6 inches tall', 'Physics and Math')
+
+print("Original Tuple: ")
+print(my_tuple)
+
+# Convert to list directly
+my_list = list(my_tuple)
+
+print("\nConverted Tuple to List:")
+print(my_list)
